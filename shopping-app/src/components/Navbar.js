f397ea7958b625a9bc22d6e0ffa660e0d8ee5fdb
Kshitij <kv6124@gmail.com>
2020-07-17 00:21:15 +0530

    Shoes Cart App Basic

---
+++ shopping-app/src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+ const Navbar = ()=>{
+    return(
+            <nav className="nav-wrapper">
+                <div className="container">
+                    <Link to="/" className="brand-logo">Shoes Cart Pvt. Ltd.</Link>
+                    
+                    <ul className="right">
+                        <li><Link to="/">Shop</Link></li>
+                        <li><Link to="/cart">My cart</Link></li>
+                        
+                    </ul>
+                </div>
+            </nav>
+   
+        
+    )
+}
+
+export default Navbar;
